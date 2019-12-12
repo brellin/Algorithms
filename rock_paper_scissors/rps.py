a67bf235632204a5a0802acf6e4507d327e58b7a
brellin <cekakid@gmail.com>
2019-12-11 18:09:19 -0600

    rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,33 @@
 
 import sys
 
+rps = [
+    ['rock'],
+    ['paper'],
+    ['scissors']
+]
+
+
 def rock_paper_scissors(n):
-  pass 
+
+    possibilities = []
+
+    def add_possiblility(n, arr):
+
+        if n == 0:
+            return possibilities.append(arr)
+
+        for possibility in rps:
+            add_possiblility(n - 1, arr + possibility)
+
+    add_possiblility(n, [])
+
+    return possibilities
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
